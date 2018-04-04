5ee4829e41ac36e16b8fbdac9cb9e4bd7449ad62
Brenda Franco and Susan Southall <susan@>
2018-04-04 16:50:05 -0700

    End of day.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
+// NOTE: Begin Business Logic
 function NewPlayer(name) {
   this.name = name;
   this.score = 0;
 }
-
-
-
-
-
-
-
-
 
 function roll() {
   var randomNumber = (Math.floor(Math.random()*6)+1);
@@ -28,21 +21,30 @@ function total(array) {
   return sum;
 }
 
+NewPlayer.prototype.turnTotal = function(total) {
+  return this.score + total;
+}
 
 // NOTE: Begins User Interface Logic.
-
 $(document).ready(function() {
+// NOTE: Player info form
 
-  $("form.player-info").submit(function(event) {
+  $("#player-info").submit(function(event) {
     event.preventDefault();
-    
-    $("form.player-info").hide();
+    var inputName1 = $("input#name1").val();
+    var inputName2 = $("input#name2").val();
+
+    var player1 = new NewPlayer(inputName1);
+    var player2 = new NewPlayer(inputName2);
+
+    $("#player-info").hide();
   });
 
   $("form#player1").submit(function(event) {
     event.preventDefault();
 
     var result = roll();
+    var currentTotal = 0;
 
     if(result === 1){
       $("#roll").html('<span class="lose">' + result + '</span>');
@@ -51,7 +53,7 @@ $(document).ready(function() {
     } else {
       $("#roll").text(result);
       currentRoll.rolls.push(result);
-      var currentTotal = total(currentRoll.rolls);
+      currentTotal = total(currentRoll.rolls);
       $("#total").text(currentTotal);
     }
 
@@ -61,10 +63,11 @@ $(document).ready(function() {
       $("#total").text("0");
       // show stats, clear arrays, play again button
     }
-
-
-
+    player1.turnTotal(currentTotal);
+    console.log(player1.score);
     $("#hold").click(function(){
+      player1.turnTotal(currentTotal);
+      console.log(player1.score);
       $("#roll").text("");
       $("#total").text("");
       currentRoll.rolls = [];
