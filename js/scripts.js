1d8d5f1963b7626338f7f131ed74599eb023a43a
Brenda Franco and Susan Southall <brenda@>
2018-04-05 11:22:34 -0700

    Clear player board on fail and add logic for win condition. MVP.

--- js/scripts.js
+++ js/scripts.js
@@ -50,14 +50,20 @@ $(document).ready(function() {
     $("#player1-board").fadeIn();
   });
 
+// player1
   $("#player1-board form").submit(function(event) {
     event.preventDefault();
     random = roll();
+
 
     if(random === 1){
       $("#roll").html('<span class="lose">' + random + '</span>');
       $("#total").html('You Lose!');
       currentRoll.rolls = [];
+      $("#player1-board").hide(1000);
+      $("#player2-board").fadeIn();
+      $("#roll").text("");
+      $("#total").text("");
     } else {
       $("#roll").text(random);
       currentRoll.rolls.push(random);
@@ -66,7 +72,7 @@ $(document).ready(function() {
     currentTotal = total(currentRoll.rolls);
     $("#total").text(currentTotal);
 
-    if(currentTotal >= 100) {
+    if(player1.score + currentTotal >= 100) {
       alert("WIN");
       currentRoll.rolls = [];
       $("#total").text("0");
@@ -81,6 +87,46 @@ $(document).ready(function() {
       currentRoll.rolls = [];
       $("#player1-board").hide();
       $("#player2-board").fadeIn();
+      $("#player1-board .current-score").text("Current Score: " + player1.score);
   });
+
+// player2
+$("#player2-board form").submit(function(event) {
+  event.preventDefault();
+  random = roll();
+
+  if(random === 1){
+    $("#roll2").html('<span class="lose">' + random + '</span>');
+    $("#total2").html('You Lose!');
+    currentRoll.rolls = [];
+    $("#player2-board").hide(1000);
+    $("#player1-board").fadeIn();
+    $("#roll2").text("");
+    $("#total2").text("");
+  } else {
+    $("#roll2").text(random);
+    currentRoll.rolls.push(random);
+  }
+
+  currentTotal = total(currentRoll.rolls);
+  $("#total2").text(currentTotal);
+
+  if(player2.score + currentTotal >= 100) {
+    alert("WIN");
+    currentRoll.rolls = [];
+    $("#total").text("0");
+  }
+  });
+
+$("#hold2").click(function(){
+    player2.turnTotal(currentTotal);
+    console.log(player2.score);
+    $("#roll2").text("");
+    $("#total2").text("");
+    currentRoll.rolls = [];
+    $("#player2-board").hide();
+    $("#player1-board").fadeIn();
+    $("#player2-board .current-score").text("Current Score: " + player2.score);
+});
 
 });
